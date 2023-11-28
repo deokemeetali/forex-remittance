f01682376aceecff751f39c5627334ad12c65122
Meetali <meetalideoke@gmail.com>
2023-11-28 14:18:38 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("");
-
   useEffect(() => {
     axios.get("http://localhost:5001/message")
       .then((res) => {
@@ -13,12 +12,10 @@ function App() {
         console.error("Error fetching message:", error);
       })
   }, []);
-
   return (
     <div className="App">
       <h1>{message}</h1>
     </div>
   );
 }
-
 export default App;
