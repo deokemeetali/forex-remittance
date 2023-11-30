b9ea6db0f4d4eff848a0a64c7eb87eb5469bd582
kshitij jogi <kshitijjogi856@gmail.com>
2023-11-30 16:52:20 +0530

    signup update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -10,19 +10,27 @@ class UserController {
   async signUp(req, res) {
     try {
       const { username, email, password } = req.body;
+
+      await this.userModel.checkUserExistence(username, email);
+
+
       const result = await this.userModel.createUser(username, email, password);
 
+     
       if (result.success) {
         res.status(201).json({ message: 'User signed up successfully' });
       } else {
         res.status(500).json({ message: result.error });
       }
     } catch (error) {
+      if (error.message === 'Username already exists' || error.message === 'Email already exists') {
+        return res.status(400).json({ message: error.message });
+      }
+
       console.error('Error signing up user:', error);
       res.status(500).json({ message: 'Error signing up user' });
     }
   }
-
   // You can add more controller methods for user-related operations here if needed
 }
 
