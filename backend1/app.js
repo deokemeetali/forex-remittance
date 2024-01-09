96576a3d4860d5de43610209c2e43b88dd279889
Meetali <meetalideoke@gmail.com>
2024-01-09 14:02:32 +0530

    update beneficiary form

--- backend1/app.js
+++ backend1/app.js
@@ -28,7 +28,7 @@ const userController = new UserController()
 app.post('/v1/auth/firebase', phoneController.managePhoneNumber);
 app.post('/signup', (req, res) => userController.signUp(req, res))
 app.post('/login', LoginController.login)
-app.get('/apI/bankDetails/:ifscCode', bankDetails.getBankDetailsByIFSC)
+app.get('/api/bankDetails/:ifscCode', bankDetails.getBankDetailsByIFSC)
 app.post('/api/beneficiaries', BeneficiaryForm.benificiaryasync)
 app.post('/sendData', sendDataController.sendData)
 app.get('/getFormData', sendDataController.getFormData)
