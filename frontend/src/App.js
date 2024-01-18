d8be99ac481d777b3ec38c3db24b5c5826e522b4
Meetali Deoke <137753304+deokemeetali@users.noreply.github.com>
2024-01-18 16:00:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import React from "react";
 import LandingPage from './components/Landingpage/landingpage'; 
-
+import { Navigate } from 'react-router-dom';
 import SignUpForm from "./userManagement/signup";
 import PhoneLogin from "./phonelogin/phonelogin";
 import LoginForm from "./loginPage/login"; 
