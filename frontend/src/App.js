7ea0a94b538c701b7bb184d501e819ee631564d7
Meetali Deoke <137753304+deokemeetali@users.noreply.github.com>
2024-01-18 15:56:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,13 @@ function App() {
       <Route path="/displayform" element={<DisplayBeneficiary/>}/>
       <Route path="/signup" element={<SignUpForm/>}/>
       <Route path="/login" element={<LoginForm />}/>
-      <Route path="/mainpage" element={<MainPages/>}/>
+      <Route path="/mainpage" element={<Navigate to="/mainpage/dashboard" />} /> 
+          <Route path="/mainpage" element={<MainPages />} >
+             <Route path="benificiaryform" element={<BeneficiaryForm />} /> 
+            <Route path="dashboard" element={<Dashboard/>} />
+            <Route path="displayform" element={<DisplayBeneficiary />} />
+            <Route path='ForexRemittance' element={<ForexRemittanceForm />} />
+          </Route>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/phonelogin" element={<PhoneLogin />} />
       <Route path='/ForexRemittance' element={<ForexRemittanceForm/>}/>
